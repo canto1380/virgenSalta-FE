e841f67fe1e18597735efa417d1482d3dac9efa7
canto1380 <atapenalba16@gmail.com>
2023-04-08 12:55:42 -0300

    add schedules page

--- src/containers/Schedules/index.js
+++ src/containers/Schedules/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
+import DailyEvents from '../../components/Schedules/DailyEvents';
+import ImportantDates from '../../components/Schedules/ImportantDates';
 
 const Schedules = () => {
   return (
-    <div>
-Container para la pagina de HORARIOS
-    </div>
+    <Container fluid className="bg-gradient-2">
+      <div className='container'>
+      <Row className='mx-3 pt-2 pb-5'>
+        <Col xs={12} className='pt-5 pb-3'>
+          <p className='page-section-title text-light'>Horarios de Celebraciones</p>
+        <hr />
+        </Col>
+        <Col xs={12} className='bg-light px-0'>
+          <ImportantDates />
+        </Col>
+        <Col xs={12} className='my-5 bg-light px-0'>
+          <DailyEvents />
+        </Col>
+      </Row>
+      </div>
+    </Container>
   );
 };
 
