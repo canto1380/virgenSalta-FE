3cbe26714f13c3244fd3be8d9ad1bf148c26894b
canto1380 <atapenalba16@gmail.com>
2023-10-22 11:08:19 -0300

    prueba url

--- src/components/PhotosGallery/index.js
+++ src/components/PhotosGallery/index.js
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
-import HeaderSections from "../Title/HeaderSections";
-import axios from "axios";
-import "./feedInstagram.css";
+import React, { useEffect, useState } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import HeaderSections from '../Title/HeaderSections'
+import axios from 'axios'
+import './feedInstagram.css'
 
 const PhotosGallery = () => {
-  const [dataFeedInsta, setDataFeedInsta] = useState();
+  const [dataFeedInsta, setDataFeedInsta] = useState()
 
   const getInstaFeed = async () => {
-    const token = process.env.REACT_APP_INSTAGRAM;
+    const token = process.env.REACT_APP_INSTAGRAM
     const fields =
-      "thumbnail_url,media_url,media_type,caption,permalink,limit=80";
-    const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}`;
+      'thumbnail_url,media_url,media_type,caption,permalink,limit=80'
+    const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}`
 
-    const { data } = await axios.get(url);
-    setDataFeedInsta(data.data);
-  };
+    const { data } = await axios.get(url)
+    setDataFeedInsta(data.data)
+    console.log(data.data)
+  }
   useEffect(() => {
-    getInstaFeed();
-  }, []);
+    getInstaFeed()
+  }, [])
 
   return (
     <div>
-      <HeaderSections title={"Instagram"} linkRef={"https://www.instagram.com/atapenalba/"} blank titleBtn={'Ver perfil'}/>
+      <HeaderSections
+        title={'Instagram'}
+        linkRef={'https://www.instagram.com/atapenalba/'}
+        blank
+        titleBtn={'Ver perfil'}
+      />
       {/* <Row className="mt-3 mb-5">
         {photosBack?.map((p) => (
           <Col key={p.id} xs={12} md={4} lg={3} className="mb-4">
@@ -35,21 +41,21 @@ const PhotosGallery = () => {
           <Col key={d.id} xs={12} md={4} className='py-3'>
             <a
               href={`${d.permalink}`}
-              target="_blank"
-              rel="noreferrer"
-              className="container-feed"
+              target='_blank'
+              rel='noreferrer'
+              className='container-feed'
             >
-              <div className="image">
-                {d.media_type === "IMAGE" ? (
-                  <img src={`${d.media_url}`} alt="" />
+              <div className='image'>
+                {d.media_type === 'IMAGE' ? (
+                  <img src={`${d.media_url}`} alt='' />
                 ) : (
                   <video controls>
                     <source src={d.media_url} />
                   </video>
                 )}
               </div>
-              <div className="opacity-hover">
-                <div className="caption">
+              <div className='opacity-hover'>
+                <div className='caption'>
                   <p>{d.caption.slice(0, 100)}</p>
                 </div>
               </div>
@@ -58,7 +64,7 @@ const PhotosGallery = () => {
         ))}
       </Row>
     </div>
-  );
-};
+  )
+}
 
-export default PhotosGallery;
+export default PhotosGallery
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 
-const url = process.env.REACT_APP_API ? process.env.REACT_APP_API : process.env.REACT_APP_PRODUCTION
+const url = process.env.REACT_APP_API
+  ? process.env.REACT_APP_API
+  : process.env.REACT_APP_PRODUCTION
+console.log(url)
 export const api = async (method, endpoint, data, token) => {
   try {
     const res = await axios({
@@ -9,13 +12,13 @@ export const api = async (method, endpoint, data, token) => {
         authorization: `${token}`,
       },
       method,
-      url: `${url}/${endpoint}`
-    });
+      url: `${url}/${endpoint}`,
+    })
     return res
   } catch (error) {
     return error
   }
-};
+}
 
 export const apiParams = async (method, params, endpoint, data, tokenAuth) => {
   try {
@@ -27,9 +30,9 @@ export const apiParams = async (method, params, endpoint, data, tokenAuth) => {
       },
       method,
       url: `${url}/${endpoint}`,
-    });
+    })
     return res
   } catch (error) {
     return error
   }
-};
+}
