8e37cf927756c51f2400337608cbfacfa685bfda
canto1380 <atapenalba16@gmail.com>
2023-10-22 12:53:57 -0300

    fix youtube error

--- src/components/Carousel/VideoCard.js
+++ src/components/Carousel/VideoCard.js
@@ -1,40 +1,50 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import "../News/news.css";
-import ReactPlayer from "react-player";
-import "../VideoGallery/videoGallery.css";
+import React, { useEffect } from 'react'
+import { Card } from 'react-bootstrap'
+import '../News/news.css'
+import '../VideoGallery/videoGallery.css'
+import axios from 'axios'
 
 const VideoCard = ({ data, i }) => {
+  useEffect(() => {
+    getVideoYoutube()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const api_key = 'AIzaSyC530nKCd3axC0fxS2yaYAIwaPnC24guZ4'
+  const url = `https://www.googleapis.com/youtube/v3/videos?key=${api_key}&id=${'YjBSl8v1u8w'}&part=snippet`
+  const getVideoYoutube = async () => {
+    console.log(url)
+    const res = await axios(url)
+    console.log(res)
+  }
   return (
-    <div className="">
-      <Card className="cardvideo-container border border-0">
+    <div className=''>
+      <Card className='cardvideo-container border border-0'>
+        {/* <iframe
+          width='100%'
+          height='200'
+          // src="https://www.youtube.com/embed/YjBSl8v1u8w?origin=https://virgendesalta.netlify.app"
+          src={`https://www.googleapis.com/youtube/v3/videos?key=${api_key}&id=${'YjBSl8v1u8w'}&part=snippet`}
+          title='Video de la Virgen de Salta'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          allowFullScreen
+        ></iframe> */}
+
         <iframe
-          width="100%"
-          height="200"
-          src="https://www.youtube.com/embed/YjBSl8v1u8w?origin=https://virgendesalta.netlify.app"
-          title="Video de la Virgen de Salta"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          width='100%'
+          height='200'
+          src='https://www.youtube.com/embed/YjBSl8v1u8w'
+          title='La Virgen del Cerro en Salta'
+          // frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
           allowFullScreen
         ></iframe>
 
-        <p className="mb-0 cardvideo-title">
-          {data?.titulo} {i + 1}
-        </p>
-      </Card>
-      <Card className="mt-5 border border-0">
-        <ReactPlayer
-          url={require("../../videos/Presentacin1 - Comprimida.mp4")}
-          // url="https://www.youtube.com/embed/YjBSl8v1u8w"
-          width="100%"
-          height="200px"
-          controls
-        />
-        <p className="mb-0 cardvideo-title">
+        <p className='mb-0 cardvideo-title'>
           {data?.titulo} {i + 1}
         </p>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default VideoCard;
+export default VideoCard
--- src/components/index.js
+++ src/components/index.js
@@ -1,58 +1,58 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import Cover from "./Cover";
-import News from "./News";
-import Categories from "./Categories";
-import Title from "./Title";
-import SpecialDays from "./SpecialDays";
-import Statics from "./Statics";
-import PhotosGallery from "./PhotosGallery";
-import VideoGallery from "./VideoGallery";
-import FastAccess from "./FastAccess";
-import Layout from "./Layout/Layout";
-import LayoutFoot from "./Layout/LayoutFoot";
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Cover from './Cover'
+import News from './News'
+import Categories from './Categories'
+import Title from './Title'
+import SpecialDays from './SpecialDays'
+import Statics from './Statics'
+import PhotosGallery from './PhotosGallery'
+import VideoGallery from './VideoGallery'
+import FastAccess from './FastAccess'
+import Layout from './Layout/Layout'
+import LayoutFoot from './Layout/LayoutFoot'
 const Home = () => {
   return (
     <div>
       <Layout />
       <Title />
-      <Container fluid className="px-0 container-cover">
+      <Container fluid className='px-0 container-cover'>
         <Cover />
       </Container>
-      <Container className="py-5">
+      <Container className='py-5'>
         <News />
-        <div className="text-center">
-          <p className="phrase1 mb-0">
+        <div className='text-center'>
+          <p className='phrase1 mb-0'>
             "Hay que juntar el rebaño antes que oscurezca"
           </p>
-          <p className="phrase1 mb-4">
+          <p className='phrase1 mb-4'>
             "No he venido a criticar ni a destruir, sino a Construir"
           </p>
         </div>
-        <div className="text-center">
-          <p className="section-title">
+        <div className='text-center'>
+          <p className='section-title'>
             Sitio Oficial del Santuario de las Apariciones de la Santísima
             Virgen María y de Nuestro Señor Jesucristo
           </p>
-          <p className="section-title">SALTA - ARGENTINA</p>
+          <p className='section-title'>SALTA - ARGENTINA</p>
         </div>
-        <hr className="my-5" />
+        <hr className='my-5' />
         <Categories />
         <FastAccess />
       </Container>
-      <Container fluid className="container-special-day py-5">
-        <SpecialDays className="px-0" />
+      <Container fluid className='container-special-day py-5'>
+        <SpecialDays className='px-0' />
       </Container>
-      <Container className="py-5">
+      <Container className='py-5'>
         <Statics />
-        <hr className="my-5" />
-        <PhotosGallery />
-        <hr className="my-5" />
+        <hr className='my-5' />
+        {/* <PhotosGallery /> */}
+        <hr className='my-5' />
         <VideoGallery />
       </Container>
       <LayoutFoot />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
