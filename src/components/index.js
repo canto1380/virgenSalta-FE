71984d50affacd2a2c9ef0ad802b74fdb915f649
canto1380 <atapenalba16@gmail.com>
2024-03-25 12:41:05 -0300

    correcion errores

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Categories from './Categories'
 import Title from './Title'
 import SpecialDays from './SpecialDays'
 import Statics from './Statics'
-import PhotosGallery from './PhotosGallery'
+// import PhotosGallery from './PhotosGallery'
 import VideoGallery from './VideoGallery'
 import FastAccess from './FastAccess'
 import Layout from './Layout/Layout'
@@ -123,7 +123,7 @@ const Home = () => {
         <hr className='my-5' />
         {/* <PhotosGallery /> */}
         <hr className='my-5' />
-        {/* <VideoGallery videosBack={videosBack} /> */}
+        <VideoGallery videosBack={videosBack} />
       </Container>
       <LayoutFoot />
     </div>
